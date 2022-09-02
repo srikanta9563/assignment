4181ac55680cc639eddbf153b4f5e46e54b2dc19
srikanta9563 <110465259+srikanta9563@users.noreply.github.com>
2022-09-02 14:21:59 +0530

    Add files via upload

---
+++ display.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class display {
+    public static void print(){
+        System.out.println("Hello my message");
+    }
+    public static void main(String[] args) {
+
+    }
+}
