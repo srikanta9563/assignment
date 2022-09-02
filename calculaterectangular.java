ab49856a89fb329dfec58ce34529645cbced2eb6
srikanta9563 <110465259+srikanta9563@users.noreply.github.com>
2022-09-02 18:58:55 +0530

    Add files via upload

---
+++ calculaterectangular.java
@@ -0,0 +1,11 @@
+package com.company;
+class Rectangle {
+    void calculatearea() {
+        System.out.println(" The area of the rectangle is calculated using formula length*breadth");
+    }
+}
+class Areacalculator{
+    public static void main(String[] args) {
+        Rectangle obr=new Rectangle();
+        obr.calculatearea();
+    }}
