9b81453971e2767d2071cc3cd70847cb31074289
srikanta9563 <110465259+srikanta9563@users.noreply.github.com>
2022-09-02 14:21:41 +0530

    Add files via upload

---
+++ Assigment.java
@@ -0,0 +1,13 @@
+ // fix the compilation error and invoke the method print message() display Message
+ // Class
+
+ package com.company;
+
+class Myprogram{
+    public static void main(String[] args) {
+       display program= new display();
+
+        program.print();
+
+    }
+}
