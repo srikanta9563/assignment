20e15944c429b34e9f25d374a4f0a6447c0bd0a9
srikanta9563 <110465259+srikanta9563@users.noreply.github.com>
2022-09-21 20:59:59 +0530

    Update Divisable.java

--- Divisable.java
+++ Divisable.java
@@ -2,18 +2,18 @@
 public class Divisable {
 
         public static void main(String args[]) {
-            System.out.println("\nDivided by 5: ");
+            System.out.println("Divided by 5: ");
             for (int i=1; i<200; i++) {
                 if (i%5==0)
                     System.out.print(i +", ");
             }
 
-            System.out.println("\n\nDivided by 7: ");
+            System.out.println("Divided by 7: ");
             for (int i=1; i<200; i++) {
                 if (i%7==0) System.out.print(i +", ");
             }
 
-            System.out.println("\n\nDivided by 5 & 7: ");
+            System.out.println("nDivided by 5 & 7: ");
             for (int i=1; i<200; i++) {
                 if (i%5==0 && i%7==0) System.out.print(i +", ");
             }
