8f001751d973e2595f402d2b418d8be87e3ed98e
srikanta9563 <110465259+srikanta9563@users.noreply.github.com>
2022-09-27 22:50:19 +0530

    Add files via upload

---
+++ avaragearray.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class avaragearray {
+    static void AvarageArray(int a [],int n){
+        int sum=0;
+        for(int i=0;i<a.length;i++) {
+
+          sum = sum+a[i];
+        }
+        int avg = sum/n;
+        System.out.println("The avarage is : "+avg);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Number of element :");
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for(int i=0;i<n;i++){
+            System.out.println("ENTER THE ELEMENT NUMBER "+i);
+            a[i]= sc.nextInt();
+        }
+        AvarageArray(a,n);
+    }
+}
