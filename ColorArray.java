7ff29604c36d0a7e6f62a1703225cf3089940f49
srikanta9563 <110465259+srikanta9563@users.noreply.github.com>
2022-09-27 22:49:38 +0530

    Add files via upload

---
+++ ColorArray.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ColorArray {
+    static void display(String arr[]){
+        System.out.println("The array is : ");
+        for(int i=0;i<arr.length;i++){
+
+            System.out.print(arr[i]+", ");
+        }}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of element :");
+        int n = sc.nextInt();
+        String arr[] = new String[n];
+        for(int i = 0;i<n;i++){
+            System.out.println("Enter the color in index of "+i);
+            arr[i]=sc.next();
+        }
+        display(arr);
+    }
+}
