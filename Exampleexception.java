f4809ddba4557bccfe9d241cee2201b98572358e
srikanta9563 <110465259+srikanta9563@users.noreply.github.com>
2022-10-19 11:40:20 +0530

    Add files via upload

---
+++ Exampleexception.java
@@ -0,0 +1,24 @@
+//You will be given two integers x and y as input, you have to compute x/y. If x and y are
+//not 32 bit signed integers or if y is zero, exception will occur and you have to report it.
+
+import java.util.*;
+public class Exampleexception {
+    public static void main(String[] args) {
+          Scanner scan = new Scanner(System.in);
+        System.out.println(" Enter the values of x and y : ");// object of scanner class
+        try { //try block
+
+            int x = scan.nextInt();
+            int y = scan.nextInt();
+            System.out.println(x / y);
+        }
+            catch(InputMismatchException e) { //using catch block trying to catch the exception if any InputMismatchException exception occurs
+            System.out.println(e.getClass().toString().replaceFirst("class ", ""));
+        } catch (ArithmeticException e) { ///using catch block trying to catch the exception if any ArithmeticException occurs
+            // Print exception
+            System.out.println(e);
+        }
+        scan.close();
+    }
+    }
+
